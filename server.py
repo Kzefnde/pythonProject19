6420540c3511a68385528d7e47e63f0965b2d188
Kzefnde <144488791+Kzefnde@users.noreply.github.com>
2024-01-31 20:16:57 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,11 +1,11 @@
+from multiprocessing import Value
 import zmq
 import cv2
 import time
 import pickle
-
 from threading import Thread
 
-def send_frames(socket, video_path, annotations, speed_factor):
+def send_frames(socket, video_path, annotations, speed_factor, shared_timestamp):
     cap = cv2.VideoCapture(video_path)
     frame_rate = cap.get(cv2.CAP_PROP_FPS)
     annotation_index = 0
@@ -19,16 +19,20 @@ def send_frames(socket, video_path, annotations, speed_factor):
 
             time.sleep((1 / frame_rate) / speed_factor)
 
-            # Отправляем данные в виде байтов
-            data = {"frame": frame, "timestamp": timestamp, "annotation_index": annotation_index}
+            data = {"frame": frame, "timestamp": timestamp, "annotation_index": annotation_index,
+                    "shared_timestamp": shared_timestamp.value}
+
             socket.send(pickle.dumps(data))
+
+            # Обновление общей метки времени
+            shared_timestamp.value = timestamp
         else:
-            cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+            break
 
 def main():
     try:
         context = zmq.Context()
-        socket = context.socket(zmq.PUSH)
+        socket = context.socket(zmq.PUB)
         socket.bind("tcp://*:8000")
 
         annotations_list = []
@@ -36,11 +40,12 @@ def main():
             annotations = open(f'{i}.txt').read().splitlines()
             annotations_list.append(annotations)
 
-        speed_factor = 1.0  # Фактор скорости по умолчанию
+        speed_factor = 1.0
+        shared_timestamp = Value('d', 0.0)  # Общая переменная для хранения метки времени
 
         threads = []
         for i, video_path in enumerate(['1.mp4', '2.mp4', '3.mp4', '4.mp4']):
-            thread = Thread(target=send_frames, args=(socket, video_path, annotations_list[i], speed_factor))
+            thread = Thread(target=send_frames, args=(socket, video_path, annotations_list[i], speed_factor, shared_timestamp))
             threads.append(thread)
 
         for thread in threads:
